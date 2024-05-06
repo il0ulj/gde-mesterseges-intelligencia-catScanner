27b1c48061158bd229584bbe03156d786cb3f0a9
Péter Birkás <il0ulj@neptun.gde.hu>
2024-05-06 21:12:36 +0200

    design created

--- catScanner/Form1.Designer.cs
+++ catScanner/Form1.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(180, 27);
+            button1.TabIndex = 0;
+            button1.Text = "Tallózás ...";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(198, 12);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(854, 27);
+            textBox1.TabIndex = 1;
+            textBox1.Text = " Tallózz be egy tetszőleges képet egy macskáról, vagy válassz egyet a példaképk közül!";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1064, 636);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "catScanner";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private ProgressBar progressBar1;
     }
 }
