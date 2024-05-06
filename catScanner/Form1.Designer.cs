c174702fb82dfe62b992470a2fe604d8c4f64377
Péter Birkás <il0ulj@neptun.gde.hu>
2024-05-06 21:13:49 +0200

    required packages installed

--- catScanner/Form1.Designer.cs
+++ catScanner/Form1.Designer.cs
@@ -40,6 +40,7 @@
             button1.TabIndex = 0;
             button1.Text = "Tallózás ...";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // textBox1
             // 
--- catScanner/Form1.cs
+++ catScanner/Form1.cs
@@ -6,5 +6,10 @@ namespace catScanner
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
